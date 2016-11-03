3303a09219b07e978188c374018ec42f26549119
Mike Foley <mike@yelof.com>
2016-11-03 16:17:50 -0400

    Encrypted vMotion Setting
    An example of setting on a per-VM basis Encrypted vMotion

---
+++ Encrypted vMotion/Encrypted vMotion - VM Config Spec snippet.ps1
@@ -0,0 +1,11 @@
+$viserver = "mgt-vc-01.lab1.local"
+$user = "administrator@vsphere.local"
+$password = "VMware1!"
+Connect-VIServer $viserver -user $user -Password $password
+$vmname = "yVM-1"
+$vm = Get-View (Get-VM $vmname).ID
+$CfgSpec = New-Object VMware.Vim.VirtualMachineConfigSpec
+$CfgSpec.extraconfig = New-Object VMware.Vim.optionvalue
+$CfgSpec.extraconfig[0].Key="migrateEncryption"
+$CfgSpec.extraconfig[0].Value="Required"
+$vm.ReconfigVM($CfgSpec)
