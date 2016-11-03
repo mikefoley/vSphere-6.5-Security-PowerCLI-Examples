9930d158d67898881f4dee2258ebbc211b6be432
Mike Foley <mike@yelof.com>
2016-11-03 16:19:21 -0400

    Added comment
    Added comment describing the values

--- Encrypted vMotion/Encrypted vMotion - VM Config Spec snippet.ps1
+++ Encrypted vMotion/Encrypted vMotion - VM Config Spec snippet.ps1
@@ -7,5 +7,6 @@ $vm = Get-View (Get-VM $vmname).ID
 $CfgSpec = New-Object VMware.Vim.VirtualMachineConfigSpec
 $CfgSpec.extraconfig = New-Object VMware.Vim.optionvalue
 $CfgSpec.extraconfig[0].Key="migrateEncryption"
+#Values are Disabled, Opportunistic and Required
 $CfgSpec.extraconfig[0].Value="Required"
 $vm.ReconfigVM($CfgSpec)
